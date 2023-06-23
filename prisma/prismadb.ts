a4ccb00c9897e18697511e89f4f94a99191edd15
Tony <greybluesea@gmail.com>
2023-06-23 19:06:16 +1200

    nextAuth  route.ts

---
+++ prisma/prismadb.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined; // eslint-disable-line @typescript-eslint/no-
+}
+
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
