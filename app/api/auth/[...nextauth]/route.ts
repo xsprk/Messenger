503dade4e70ab9b43a14f1121a3863b1bd497f89
Tony <greybluesea@gmail.com>
2023-06-29 19:12:30 +1200

    'text-center' for EmptyPanel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,5 @@
-import bcrypt from "bcrypt";
-import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-import prisma from "@/prisma/prismadb";
 import { authOptions } from "@/app/utils/authOptions";
 
 const handler = NextAuth(authOptions);
--- app/components/asides/EmptyPanel.tsx
+++ app/components/asides/EmptyPanel.tsx
@@ -6,14 +6,16 @@ const EmptyPanel = (props: Props) => {
   return (
     <div
       className="
+  px-4
   
+  lg:px-8
   min-h-screen 
   grid
   place-items-center
   
   "
     >
-      <h3 className=" text-xl  ">
+      <h3 className=" text-xl text-center ">
         Select a previous conversation or start a new chat
       </h3>
     </div>
